254d329f41836e057801d6fe181b67e3f3074125
warleyzee <warleyzee@hotmail.com>
2022-04-26 16:09:14 +0100

    [new] - File

--- recipe/views.py
+++ recipe/views.py
@@ -1,31 +1,36 @@
 from unicodedata import name
-from django.http import Http404
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404, get_object_or_404
 from util.recipes.factory import make_recipe
 from .models import Receita
 
 # Create your views here.
 
 def home(request):
-    recipe = Receita.objects.filter(is_published = True).order_by('-id')
+    recipe = Receita.objects.filter(is_published=True).order_by('-id')
     return render(request, 'recipe/pages/home.html', context={
         'recipes' : recipe
     })
 
 def category(request, category_id):
-    recipe = Receita.objects.filter(category__id=category_id, is_published=True).order_by('-id')
-
-    if not recipe:
-        raise Http404('Not Found')
-
+    recipes = get_list_or_404(
+        Receita.objects.filter(
+            category__id=category_id,
+            is_published=True,
+        ).order_by('-id')
+    )
 
     return render(request, 'recipe/pages/category.html', context={
-        'recipes' : recipe,
-        'title': f'{recipe.first().category.name}  - Category | '
+        'recipes': recipes,
+        'title': f'{recipes[0].category.name} - Category | '
     })
 
 def recipe(request, id):
+    recipe = Receita.objects.filter(
+        pk=id,
+        is_published = True,
+    ).order_by('-id').first()
+ 
     return render(request, 'recipe/pages/recipe-view.html', context={
-        'recipe' : make_recipe(),
+        'recipe' : recipe,
         'is_detail_page': True,
     })
