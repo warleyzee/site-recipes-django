a6278e5f1767a76a66a30cd48e63ec7f0710e40e
warleyzee <warleyzee@hotmail.com>
2022-04-28 15:50:52 +0100

    Tentando voltar o site

--- recipe/admin.py
+++ recipe/admin.py
@@ -1,5 +1,4 @@
 from django.contrib import admin
-<<<<<<< HEAD
 from .models import Category, Recipes
 # Register your models here.
 
@@ -11,7 +10,3 @@ class ReceitaAdmin(admin.ModelAdmin):
     ...
 
 admin.site.register(Category, CategoryAdmin)
-=======
-
-# Register your models here.
->>>>>>> c0f2431b99c704a27262d6a5d7e81d675cbb0e24
